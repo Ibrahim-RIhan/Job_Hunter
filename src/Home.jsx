8dcbf80a9cccf018cf0ec797a1217d5a7a934d0f
rihan010 <ibrhimrihan000@gmail.comm>
2023-07-17 22:02:33 +0600

    final

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 const Home = () => {
-    return (
-        <div className='container mx-auto mt-12  '>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-          
-         
-        </div>
-    );
+  return (
+    <div>
+      <div className="container mx-auto mt-12  ">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
